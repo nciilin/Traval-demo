396312bcd695b44d19dc7a0dca8d6436a784e712
vincentcheng <670700452@qq.com>
2019-11-24 16:33:29 +0800

    finish city css

--- Traval/src/pages/city/City.vue
+++ Traval/src/pages/city/City.vue
@@ -2,19 +2,24 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-aphabet></city-aphabet>
   </div>
 </template>
 
 <script>
-
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAphabet from './components/Alphabet'
 
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAphabet
   }
 }
 </script>
