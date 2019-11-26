188447f2b8a9ce6bc91e5a8e44640188fc268d05
vincentcheng <670700452@qq.com>
2019-11-26 23:47:10 +0800

    fix dist

--- Traval/config/index.js
+++ Traval/config/index.js
@@ -57,7 +57,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Traval-demo/Traval/dist/',
+    assetsPublicPath: '/Traval-demo/Traval/dist',
 
     /**
      * Source Maps
